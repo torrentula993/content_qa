9a80c3b924cc1961b9b85679d29e953ba31775aa
Bailey Wier <bailey@spotify.com>
2011-11-02 12:31:06 -0400

    despotify

---
+++ utils/despotify.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#remove client stuff
+rm -rf ~/Library/Caches/com.spotify.client
+rm -rf ~/Library/Application\ Support/Spotify/
+rm -rf /Applications/Spotify.app/
+
+#remove Flash cookies (FB)
+rm -rf ~/Library/Preferences/Macromedia/Flash\ Player/#SharedObjects/*
+rm -rf ~/Library/Preferences/Macromedia/Flash\ Player/macromedia.com/support/flashplayer/sys/*
